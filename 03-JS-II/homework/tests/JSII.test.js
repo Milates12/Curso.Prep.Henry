a89b4b54ddd7d93ca2214825c72202487ac93f11
Your Name <you@example.com>
2020-11-13 14:02:29 -0500

    Modificando el tests de JSII

--- 03-JS-II/homework/tests/JSII.test.js
+++ 03-JS-II/homework/tests/JSII.test.js
@@ -1,109 +1,81 @@
-/* eslint-disable no-undef */
-const {
-  obtenerMayor,
-  saludo,
-  esDiezOCinco,
-  estaEnRango,
-  esEntero,
-  fizzBuzz,
-  esPrimo
-} = require('../homework');
 
-describe('obtenerMayor(x, y)', function() {
-  it('should return x if it is larger than y', function() {
-    expect(obtenerMayor(10, 5)).toBe(10);
-  });
-  it('should return y if it is larger than x', function() {
-    expect(obtenerMayor(50, 100)).toBe(100);
-  });
-  it('should return either one if they are the same', function() {
-    expect(obtenerMayor(1000, 1000)).toBe(1000);
-  });
-});
+function obtenerMayor(x, y) {
+  if(x > y) {
+    return x;
+    //Si "x" es mayor que "y" retornará "x"
+  }
+  return y;
+  //si "y" es mayor que "x" retornará "y"
+  //si "x" y "y" son iguales, retorna cualquiera de los dos 
+}
 
-describe('saludo(idioma)', function() {
-  it('should return \'Guten Tag!\' for aleman', function() {
-    expect(saludo('aleman')).toBe('Guten Tag!');
-  });
-  it('should return \'Hola!\' for ingles', function() {
-    expect(saludo('ingles')).toBe('Hello!');
-  });
-  it('should return \'Ni Hao!\' for mandarin', function() {
-    expect(saludo('mandarin')).toBe('Ni Hao!');
-  });
-  it('should return \'Hola!\' if no argument is passed in or if a idioma beyond ingles, mandarin, and aleman is passed in.', function() {
-    expect(saludo('frances')).toBe('Hola!');
-    expect(saludo()).toBe('Hola!');
-  });
-});
+function saludo(idioma) {
+  if (idioma === 'aleman') {
+    return 'Guten Tag!';
+    // Si elijo idioma 'aleman', me mostrará el saludo 'Guten Tag!'
+  } else if (idioma ==='ingles') {
+    return 'Hello!';
+    // Si elijo idioma 'ingles', me mostrará el saludo 'Hello!'
+  } else if (idioma ==='mandarin') {
+    return 'Ni Hao!';
+    // Si elijo idioma 'mandarin', me mostrará el saludo 'Ni Hao!'
+  } else {
+    return 'Hola!';
+    // Si hay una opción que no existe, me mostrará el saludo 'Hola!'
+  }
+}
 
-describe('esDiezOCinco(num)', function() {
-  it('should return true if num is 10 or 5', function() {
-    expect(esDiezOCinco(10)).toBe(true);
-    expect(esDiezOCinco(5)).toBe(true);
-  });
-  it('should return false if num is not 10 or 5', function() {
-    expect(esDiezOCinco(11)).toBe(false);
-    expect(esDiezOCinco(6)).toBe(false);
-    expect(esDiezOCinco(0)).toBe(false);
-    expect(esDiezOCinco(5.01)).toBe(false);
-  });
-});
+function esDiezOCinco(num) {
+  if (num === 10 || num === 5) {
+    return true;
+    //Si el num es 10 o 5 me retornará true
+  } else {
+    return false;
+    // Si el num no es 10 o 5 retornará false
+  }
+}
 
-describe('estaEnRango(num)', function() {
-  it('should return true if num is inside range', function() {
-    expect(estaEnRango(35.5)).toBe(true);
-    expect(estaEnRango(40)).toBe(true);
-    expect(estaEnRango(49)).toBe(true);
-    expect(estaEnRango(21)).toBe(true);
-  });
-  it('should return false if outside of range', function() {
-    expect(estaEnRango(10)).toBe(false);
-    expect(estaEnRango(20)).toBe(false);
-    expect(estaEnRango(50)).toBe(false);
-    expect(estaEnRango(100)).toBe(false);
-  });
-});
+function estaEnRango(num) {
+  if (num > 20 && num < 50) {
+    return true;
+    //Si el num está entre 20 y 50 (no incluidos estos mismos valores) retornará true
+  } else {
+    return false;
+    //Si el num no está entre 20 y 50 (no incluidos estos mismos valores) retornará false
+  }
+}
 
-describe('esEntero(num)', function() {
-  it('should return true if num is 5', function() {
-    expect(esEntero(5)).toBe(true);
-  });
-  it('should return false if num is 0.5', function() {
-    expect(esEntero(0.5)).toBe(false);
-  });
-  it('should return true if num is -20', function() {
-    expect(esEntero(-20)).toBe(true);
-  });
-  it('should return true for 0', function() {
-    expect(esEntero(0)).toBe(true);
-  });
-});
+function esEntero(num) {
+  if (num % 1 === 0) {
+    return true;
+    //si el num es entero retornará true
+  } else {
+    return false;
+    // si num no es entero retornará false
+  }
+}
 
-describe('fizzBuzz(num)', function() {
-  it('should return fizz if divisible by 3', function() {
-    expect(fizzBuzz(9)).toBe('fizz');
-  });
-  it('should return buzz if divisible by 5', function() {
-    expect(fizzBuzz(10)).toBe('buzz');
-  });
-  it('should return fizzbuzz if divisible by 3 and 5', function() {
-    expect(fizzBuzz(15)).toBe('fizzbuzz');
-  });
-  it('should return num if not divisible by 3 or 5', function() {
-    expect(fizzBuzz(4)).toBe(4);
-  });
-});
+function fizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+    // Si el num es divisible entre 3 y 5, retornará fizzbuzz
+  } else if (num % 3 === 0) {
+    return 'fizz';
+    // Si el num es divisible entre 3 solamente retornará fizz
+  } else if (num % 5 === 0) {
+    return 'buzz';
+    // Si el numero es divisibe entre 5 solamente retornará buzz
+  } else if (num % 3 !== 0 && num % 5!== 0) {
+    return num; 
+    //Si el num no es divisible entre ninguno (3 o 5) retornará el num
+  }
+}
 
-describe('esPrimo(num)', function() {
-  it('should return true if num is prime', function() {
-    expect(esPrimo(7)).toBe(true);
-    expect(esPrimo(97)).toBe(true);
-  });
-  it('should return false if num is not prime', function() {
-    expect(esPrimo(10)).toBe(false);
-    expect(esPrimo(100)).toBe(false);
-    expect(esPrimo(0)).toBe(false);
-    expect(esPrimo(1)).toBe(false);
-  });
-});
+function esPrimo(num) {
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num !== 1;
+}
